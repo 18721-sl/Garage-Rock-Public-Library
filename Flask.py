c46cd397ac9aee3681de84e43bf92eea7e59a9f7
st11lock <18721@burnside.school.nz>
2020-06-19 13:42:25 +1200

    NEW

--- Flask.py
+++ Flask.py
@@ -1,4 +1,4 @@
-import Flask
+from Flask  import Flask,g
 
 import sqlite3
 
@@ -6,5 +6,14 @@ app = Flask(__name__)
 
 DATABASE = 'database.db'
 
-def get_db()
+def get_db():
+    db= getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database' , None)
+    if db is not None:
+        db.close()
